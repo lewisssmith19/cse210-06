68177307eda0a4ec03cfa17a36d87705b5f03cc1
lewisssmith19 <97068557+lewisssmith19@users.noreply.github.com>
2022-03-17 20:43:07 -0600

    Add files via upload
    Added files from Cycle game

---
+++ CSE210-05/cycles/__main__.py
@@ -0,0 +1,43 @@
+import constants
+from game.casting.cast import Cast
+from game.casting.food import Food
+from game.casting.score import Score
+from game.casting.cycle import Cycle
+from game.scripting.script import Script
+from game.scripting.control_actors_action import ControlActorsAction
+from game.scripting.move_actors_action import MoveActorsAction
+from game.scripting.handle_collisions_action import HandleCollisionsAction
+from game.scripting.draw_actors_action import DrawActorsAction
+from game.directing.director import Director
+from game.services.keyboard_service import KeyboardService
+from game.services.video_service import VideoService
+from game.shared.color import Color
+from game.shared.point import Point
+
+
+def main():
+    
+    # create the cast
+    cast = Cast()
+    cast.add_actor("foods", Food())
+    cast.add_actor("cycle1", Cycle(constants.RED, -5 * constants.CELL_SIZE))
+    cast.add_actor("cycle2", Cycle(constants.BLUE, 5 * constants.CELL_SIZE))
+    cast.add_actor("score1", Score('Player 1', Point(0,0)))
+    cast.add_actor("score2", Score('Player 2', Point(765,0)))
+   
+    # start the game
+    keyboard_service = KeyboardService()
+    video_service = VideoService()
+
+    script = Script()
+    script.add_action("input", ControlActorsAction(keyboard_service))
+    script.add_action("update", MoveActorsAction())
+    script.add_action("update", HandleCollisionsAction())
+    script.add_action("output", DrawActorsAction(video_service))
+    
+    director = Director(video_service)   
+    director.start_game(cast, script)
+
+
+if __name__ == "__main__":
+    main()
---
+++ CSE210-05/cycles/game/casting/cycle.py
@@ -0,0 +1,73 @@
+import constants
+from game.casting.actor import Actor
+from game.shared.point import Point
+
+
+class Cycle(Actor):
+    """
+    A long limbless reptile.
+    
+    The responsibility of Snake is to move itself.
+
+    Attributes:
+        _segments (array): The segments contained in the snake.
+        _offset (int): The offset in the initial coords where the snake will be drawed.
+        _color (int): The color of the snake.
+    """
+    def __init__(self, color, offset):
+        super().__init__()
+        self._segments = []
+        self._offset = offset
+        self._color = color
+        self._prepare_body()
+        
+
+    def get_segments(self):
+        return self._segments
+
+    def move_next(self):
+        # move all segments
+        for segment in self._segments:
+            segment.move_next()
+        # update velocities
+        for i in range(len(self._segments) - 1, 0, -1):
+            trailing = self._segments[i]
+            previous = self._segments[i - 1]
+            velocity = previous.get_velocity()
+            trailing.set_velocity(velocity)
+
+    def get_head(self):
+        return self._segments[0]
+
+    def grow_trail(self, number_of_segments):
+        for i in range(number_of_segments):
+            tail = self._segments[-1]
+            velocity = tail.get_velocity()
+            offset = velocity.reverse()
+            position = tail.get_position().add(offset)
+            
+            segment = Actor()
+            segment.set_position(position)
+            segment.set_velocity(velocity)
+            segment.set_text("")
+            segment.set_color(self._color)
+            self._segments.append(segment)
+
+    def turn_head(self, velocity):
+        self._segments[0].set_velocity(velocity)
+    
+    def _prepare_body(self):
+        x = int(constants.MAX_X / 2  + self._offset)
+        y = int(constants.MAX_Y / 2)
+
+        for i in range(constants.SNAKE_LENGTH):
+            position = Point(x - i * constants.CELL_SIZE, y)
+            velocity = Point(1 * constants.CELL_SIZE, 0) 
+            text = "[(o)]" if i == 0 else ""
+
+            segment = Actor()
+            segment.set_position(position)
+            segment.set_velocity(velocity)
+            segment.set_text(text)
+            segment.set_color(self._color)
+            self._segments.append(segment)

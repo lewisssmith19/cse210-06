282e668572b14b067774797c2a62e87b9663d297
MonicaBarreraC <u653058995@byui.edu>
2022-03-31 02:46:48 -0600

    Added attribute lock in Food

--- CSE210-05/cycles/game/casting/food.py
+++ CSE210-05/cycles/game/casting/food.py
@@ -17,18 +17,29 @@ class Food(Actor):
         "Constructs a new Food."
         super().__init__()
         self._points = 0
+        self._lock = False
         self.set_text(constants.COIN_SYMBOL)
         self.set_color(constants.COIN_COLOR)
         self.reset()
         
     def reset(self):
         """Selects a random position and points that the food is worth."""
-        self._points = random.randint(1, 8)
-        x = random.randint(1, constants.COLUMNS - 1)
-        y = random.randint(1, constants.ROWS - 1)
-        position = Point(x, y)
-        position = position.scale(constants.CELL_SIZE)
-        self.set_position(position)
+        if self._lock:
+            self._points = 0
+            self.reset_velocity()
+        else:
+            self._points = random.randint(1, 8)
+            x = random.randint(1, constants.COLUMNS - 1)
+            y = random.randint(1, constants.ROWS - 1)
+            position = Point(x, y)
+            position = position.scale(constants.CELL_SIZE)
+            self.set_position(position)
+
+    def lock(self):
+        """
+        Locks the food in the same place
+        """
+        self._lock = True
         
     def get_points(self):
         """Gets the points the food is worth.
